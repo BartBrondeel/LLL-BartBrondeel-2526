2aa72b03ac3f3beb52da755946d6128b0cb847a6
BartBrondeel <bart.brondeel@student.odisee.be>
2026-06-01 18:08:15 +0200

    Upload: Python Mega Course: Build 20 Real-World Apps and AI Agents Day 30

---
+++ portfolio/Weather_Forecast_App/backend.py
@@ -0,0 +1,15 @@
+import requests
+
+API_KEY="9ad264761bba1f8058679f0b7a3b8518"
+
+def get_data(place, forecast_days=None):
+    url =f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+if __name__ =="__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
